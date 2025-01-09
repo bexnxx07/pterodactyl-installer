5037f7558c5103add9ef4397fb56889d2aaa25d4
bexnxx07 <bexpuqimak@outlook.com>
2025-01-09 18:09:42 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -43,7 +43,7 @@ fi
 
 # Always remove lib.sh, before downloading it
 [ -f /tmp/lib.sh ] && rm -rf /tmp/lib.sh
-curl -sSL -o /tmp/lib.sh https://raw.githubusercontent.com/bexnxx07/pterodactyl-installer/master/lib/lib.sh
+curl -sSL -o /tmp/lib.sh https://raw.githubusercontent.com/bexnxx07/pterodactyl-installer/refs/heads/master/lib/lib.sh
 # shellcheck source=lib/lib.sh
 source /tmp/lib.sh
 
